92c61cf09a9d34f7f7c27a9bf8e020198347907a
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2023-10-29 15:19:00 +0100

    update new solution

--- my wc.js
+++ my wc.js
@@ -14,7 +14,7 @@ const log = console.log;
 */
 const args = process.argv;
 const argsLength = args.length;
-let options;
+let userOptions;
 let fileName;
 let text;
 let buffer;
@@ -28,6 +28,8 @@ const OPTIONS = {
     debug: "-debug",
     help: "-help",
 };
+const OK_EXIT = 0;
+const ERROR_EXIT = 1;
 
 function formatNumber(val) {
     return formatter.format(val);
@@ -55,7 +57,7 @@ ${OPTIONS["word"]}      to output number of words in FILE
 ${OPTIONS["multi-byte"]}      to outputs number of \\u0000-\\uFFFF
         unicode characters in FILE
 ${OPTIONS["debug"]}  to outuput developmmnt debug information
-${options["help"]}   to print this help information
+${userOptions["help"]}   to print this help information
 
 FILE, can be either
 
@@ -70,7 +72,7 @@ function printDebug() {
     log(`*** debug information ***`);
     log({
         args,
-        options,
+        userOptions,
         fileName,
         "buffer instanceof Buffer": buffer instanceof Buffer,
     });
@@ -78,3 +80,19 @@ function printDebug() {
 == end of debug information ==
     `);
 }
+
+/* parse cli arguments */
+
+/* system error */
+if (argsLength < 2) {
+    log(`System Error related to handling command line arguments,
+length of arguments should be at least 2,
+however currnet arguments length is {argsLength},
+process.argv = {args}`);
+    process.exit(1);
+}
+
+log();
+log("My wc (word count among other things) utility");
+log("by Abdullah Fatota - 2023");
+log();
